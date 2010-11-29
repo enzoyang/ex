e8670141d650cd8dab97b9170a306d810dcc8f32
Enzo Yang <enzo@enzo.(none)>
2010-11-29 13:03:32 +0800

    change country settings

--- src/explatform/settings.py
+++ src/explatform/settings.py
@@ -21,12 +21,12 @@ DATABASE_PORT = '3306'             # Set to empty string for default. Not used w
 # although not all variations may be possible on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'China/Chongqing'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
 SITE_ID = 1
 
@@ -78,4 +78,5 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'explatform.account'
 )
